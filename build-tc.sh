7a95a9beb7497148f6311afa323d55d26b054f9a
Julian Liu <wlootlxt123@gmail.com>
2019-12-22 03:46:17 +0800

    build-tc.sh: Changes for personal usage

--- build-tc.sh
+++ build-tc.sh
@@ -9,40 +9,25 @@ function msg() {
     echo -e "\e[1;32m$@\e[0m"
 }
 
-# Configure LLVM build based on environment or arguments
-if [[ "$1" == "--ci" ]] || [[ "$DRONE" == "true" ]]; then
-	msg "Configuring reduced LLVM build for CI..."
-	llvm_args=(--targets "ARM;AArch64;X86" --shallow-clone)
-	binutils_args=(--targets arm aarch64 x86_64 --shallow-clone)
-else
-	msg "Configuring full-fledged LLVM build..."
-	llvm_args=(--targets "ARM;AArch64;X86" --lto full)
-	binutils_args=(--targets arm aarch64 x86_64)
-fi
+# Configure LLVM build
+msg "Configuring full-fledged LLVM build..."
+llvm_args=(--targets "ARM;AArch64;X86")
+binutils_args=(--targets arm aarch64 x86_64)
 
 # Build LLVM
 msg "Building LLVM..."
 ./build-llvm.py \
-	--clang-vendor "Proton" \
+	--clang-vendor "LiuNian-$(date +%Y%m%d)" \
 	--projects "clang;compiler-rt;lld;polly" \
-	--pgo \
+	--incremental \
+	--build-stage1-only \
+	--install-stage1-only \
 	"${llvm_args[@]}"
 
 # Build binutils
 msg "Building binutils..."
 ./build-binutils.py \
 	"${binutils_args[@]}"
-
-# Remove unused products
-msg "Removing unused products..."
-rm -fr install/include
-rm -f install/lib/*.a install/lib/*.la
-
-# Strip remaining products
-msg "Stripping remaining products..."
-for f in $(find install -type f -exec file {} \; | grep 'not stripped' | awk '{print $1}'); do
-	strip ${f: : -1}
-done
 
 # Set executable rpaths so setting LD_LIBRARY_PATH isn't necessary
 msg "Setting library load paths for portability..."
